d6f16a332c4c477b95a5c634817ff98c63a48332
olivia <olivia.brantley9@gmail.com>
2023-03-31 16:05:19 -0700

    made the home component

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import MainHome from "./home_components/MainHome";
+import MainInfo from "./home_components/MainInfo";
+
+function Home(){
+    return(
+        <div>
+            <MainHome />
+            {/* <MainInfo /> */}
+        </div>
+    )
+}
+
+
+export default Home
