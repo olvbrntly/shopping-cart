58bcd0f7073110161215c227f512217e8f9bb53d
olivia <olivia.brantley9@gmail.com>
2023-03-31 16:04:03 -0700

    broke home page into components

---
+++ src/components/home_components/MainHome.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Home.css'
+
+function MainHome(){
+    return(
+        <div className="photo-div">
+            <h3>It's Up To The Dogs</h3>
+            <buttons>Shop Now!</buttons>
+        </div>
+    )
+}
+export default MainHome
---
+++ src/components/home_components/MainInfo.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function MainInfo(){
+    return(
+        <div>
+            this is main info
+        </div>
+    )
+}
+export default MainInfo
