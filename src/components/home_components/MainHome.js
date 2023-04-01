2aba1dbd36cd1aada29634284fcecdf931d3192b
olivia <olivia.brantley9@gmail.com>
2023-03-31 17:06:21 -0700

    changing computers

--- src/components/home_components/MainHome.js
+++ src/components/home_components/MainHome.js
@@ -1,11 +1,17 @@
 import React from "react";
 import './Home.css'
+//import {dalmation} from './images/dalmation.png'
 
 function MainHome(){
     return(
         <div className="photo-div">
-            <h3>It's Up To The Dogs</h3>
-            <buttons>Shop Now!</buttons>
+            <div className="m-Left" >
+                <h3>It's Up To The Dogs</h3>
+                <buttons>Shop Now!</buttons>
+            </div>
+            <div className="m-Right">
+                <img src={require('./images/dalmation-pup.jpg')} alt="dalmation pup" />
+            </div>
         </div>
     )
 }
