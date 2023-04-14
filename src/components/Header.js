d4ed02aeac66638bb01c6292151035347526a939
olive <olivia.brantley9@gmail.com>
2023-04-13 19:52:15 -0700

    updated header to work w react router

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,30 @@
 import React from "react";
 import './Header.css'
+import {Link} from 'react-router-dom'
 
 function Header(){
+
+    const navStyle = {
+        textDecoration: 'none',
+    }
     return(
         <div className="header">
-            <h1 className="company-name">Pongo & Perdita</h1>
+            <Link to={'/'} style={navStyle}>
+                <h1 className="company-name">Pongo & Perdita</h1>
+            </Link>
             <ul className="header-links">
-                <li>Home</li>
-                <li>Shop</li>
-                <li>About</li>
-                <li>Cart</li>
+                <Link to="/" style={navStyle}>
+                    <li className="header-links">Home</li>
+                </Link>
+                <Link to={'/shop'} style={navStyle}>
+                    <li className="header-links">Shop</li>
+                </Link>
+                <Link to={'/about'} style={navStyle}>
+                    <li className="header-links">About</li>
+                </Link>
+                <Link to={'./cart'} style={navStyle}>
+                    <li className="header-links">Cart</li>
+                </Link>
             </ul>
         </div>
     )
