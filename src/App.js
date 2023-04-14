f0aa888d670ed8862a7ea2a850e01581d9641e5a
olive <olivia.brantley9@gmail.com>
2023-04-13 19:51:51 -0700

    added routes/router etc.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
+import React from 'react';
+import 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Routes} from 'react-router-dom';
+
 import './App.css';
 import Header from './components/Header'
 import Home from './components/Home';
+import Shop from './components/Shop';
+import About from './components/About';
+import Cart from './components/Cart';
+
+
 
 function App() {
   return (
-    <div className="App">
-     <Header/>
-     <Home/>
-    </div>
+    <Router>
+      <div className="App">
+        <Header/>
+        <Routes>
+          <Route path="/" Component={Home}/>
+          <Route path="/about" Component={About}/>
+          <Route path="/shop" Component={Shop}/>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
