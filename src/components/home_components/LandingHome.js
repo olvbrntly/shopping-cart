0c847422d64b7401148e53cb53cc7de995127788
olive <olivia.brantley9@gmail.com>
2023-04-13 19:51:33 -0700

    added Link to shop button

--- src/components/home_components/LandingHome.js
+++ src/components/home_components/LandingHome.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './Home.css'
+import {Link} from 'react-router-dom'
 
 function LandingHome(){
     return(
@@ -8,7 +9,9 @@ function LandingHome(){
                 <div className="hero-text">
                     <h1>Pet Shop</h1>
                     <p>Doggo ipsum blop very good spot lotsa wags</p>
-                    <button>Shop Now</button>
+                    <Link to={'./shop'}>
+                        <button>Shop Now</button>
+                    </Link>
                 </div>
             </div>
         </div>
