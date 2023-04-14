cc237c14dd90b90f6fd68b408b9f8967adc479b0
olive <olivia.brantley9@gmail.com>
2023-04-13 19:52:59 -0700

    basic component

---
+++ src/components/about_components/Story.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './about.css'
+
+function Story(){
+    return(
+        <div>
+            <div className="about-story">
+                <h1>Our Story</h1>
+                <p>Puppy kitty ipsum dolor sit good dog foot stick canary. Teeth Mittens grooming vaccine walk swimming nest good boy furry tongue heel furry treats fish. Cage run fast kitten dinnertime ball run foot park fleas throw house train licks stick dinnertime window. Yawn litter fish yawn toy pet gate throw Buddy kitty wag tail ball groom crate ferret heel wet nose Rover toys pet supplies. Bird Food treats tongue lick teeth ferret litter box slobbery litter box crate bird small animals yawn small animals shake slobber gimme five toys polydactyl meow.</p>
+            </div>
+        </div>
+    )
+}
+
+export default Story
