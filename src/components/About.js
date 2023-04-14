c4f7ba608e235b93038f8c91ea3c19ad11b6d76b
olive <olivia.brantley9@gmail.com>
2023-04-13 19:52:50 -0700

    basic funcitonal components

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Story from './about_components/Story'
+import './About.css'
+
+
+function About(){
+    return(
+        <div className="about-div">
+            <Story />
+        </div>
+    )
+}
+
+export default About
---
+++ src/components/Shop.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+function Shop(){
+    return(
+        <h1>Shop page</h1>
+    )
+}
+
+export default Shop
