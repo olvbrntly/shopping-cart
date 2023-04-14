f61aa130a3e5efb64914f95fc7f9918697d9965c
olive <olivia.brantley9@gmail.com>
2023-04-13 21:23:56 -0700

    made contact component

---
+++ src/components/about_components/Contact.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+function Contact(){
+    return(
+        <div className="about-contact">
+            <h1>Contact</h1>
+            <div className="contact-info">
+                <p> <b> Phone:</b> 555-555-5555</p>
+                <p> <b> Email:</b> contact@pongoandperdita.com</p>
+                <p> <b> Address:</b> 101 Dalmation St. London 10101</p>
+            </div>
+
+        </div>
+    )
+}
+
+export default Contact
