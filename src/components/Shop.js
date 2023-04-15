7ef545776d10558fcf8f13ff8b27fe920ff2cea7
olivia <olivia.brantley9@gmail.com>
2023-04-14 18:54:50 -0700

    3 sample products rendering, not styled

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -8,16 +8,19 @@ function Shop(){
         {
             id:1,
             name:'banana-pjs',
+            description:'banana print puppy pajamas',
             price:'$20',
         },
         {
             id:2,
             name:'chicken-toy',
+            description:'chicken plush chew toy',
             price:'$7',
         },
         {
             id:3,
             name:'collar-bow',
+            description:'cute bow collar accessory',
             price:'$4',
         }
     ]
@@ -25,10 +28,13 @@ function Shop(){
 
 
     return(
-        <div>
-           <Products />
-           <Products />
-           <Products />
+        <div className="product-container">
+           {products.map(p =>(
+            <div key={p.id}>
+                <Products description={p.description} price={p.price}  name={p.name}/>
+            </div>
+           )
+           )}
         </div>
     )
 }
--- src/components/shop_components/Products.js
+++ src/components/shop_components/Products.js
@@ -1,9 +1,20 @@
 import React from "react";
+import './Shop.css'
 
 
-function Products(){
+function Products({description, price, name}){
     return(
-        <div></div>
+        <div>
+           
+             <img className="product-img" src={require(`./products_images/${name}.jpg`)} alt={name}/>
+         
+            <div className="product-info">
+                <p> {description}</p>
+                <p>{price}</p>
+            </div>
+            <button className="add-to-cart">Add to Cart</button>
+
+        </div>
     )
 }
 
