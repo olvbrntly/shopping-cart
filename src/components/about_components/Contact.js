26db1e64e8f19f2e2db3182b16920d5f2c4c58ff
olivia <olivia.brantley9@gmail.com>
2023-04-14 20:07:39 -0700

    edited contact

--- src/components/about_components/Contact.js
+++ src/components/about_components/Contact.js
@@ -8,7 +8,7 @@ function Contact(){
             <div className="contact-info">
                 <p> <b> Phone:</b> 555-555-5555</p>
                 <p> <b> Email:</b> contact@pongoandperdita.com</p>
-                <p> <b> Address:</b> 101 Dalmation St. London 10101</p>
+                <p> <b> Address:</b> 101 Dalmation St. London </p>
             </div>
 
         </div>
