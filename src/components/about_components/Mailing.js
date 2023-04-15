516148e288c79a12a8b487b2a1f0e23554278ec2
olivia <olivia.brantley9@gmail.com>
2023-04-14 20:07:52 -0700

    styled mailing list btn

--- src/components/about_components/Mailing.js
+++ src/components/about_components/Mailing.js
@@ -24,7 +24,7 @@ function Mailing(){
                 </div>
 
 
-                <button>Submit!</button>
+                <button className="mailing-submit">Submit!</button>
             </div>
         </div>
     )
