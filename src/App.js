9e654a1f18bd9453a40ca2a9a50377fc95c74cf1
olivia <olivia.brantley9@gmail.com>
2023-04-20 19:29:37 -0700

    commented cart

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import 'react-router-dom';
-import { BrowserRouter as Router, Switch, Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 import './App.css';
 import Header from './components/Header'
 import Home from './components/Home';
 import Shop from './components/Shop';
 import About from './components/About';
-import Cart from './components/Cart';
+//import Cart from './components/Cart';
 
 
 
